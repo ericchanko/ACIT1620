ded350c0edf315842fa23891b7faa142f432c3d5
ericchanko <ericchanko@gmail.com>
2020-11-12 15:02:41 -0800

    Modified code to check for valid course code

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,15 +5,24 @@ let dataB = {code: 'ACIT 1630', name: 'Database Systems'};
 
 const courseList = [webTech, sysAd, dataB];
 
-let courseCode = prompt("Enter a course code");
 let succesfullyFound = false;
+let validCourseCode = false;
 
-for (let i =0; i < courseList.length; i++) {
-    if (courseCode===courseList[i].code) {
+while (!validCourseCode) {
+    let courseCode = prompt("Enter a course code");
+    if (courseCode.length === 4 && !isNaN(parseInt(courseCode))) {
+        validCourseCode = true;
+    }
+}
+
+for (let i = 0; i < courseList.length; i++) {
+    if (courseList[i].code.includes(courseCode)) {
         console.log(`Yes I am taking the course: ${courseList[i].code} - ${courseList[i].name}`);
         succesfullyFound = true;
     }
 }
+
+
 
 if (!succesfullyFound) {
     let newObj = {code: prompt, name:null};
