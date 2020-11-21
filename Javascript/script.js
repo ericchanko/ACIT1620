4033d77d868945476c363f5712a4092135d6ca20
ericchanko <ericchanko@gmail.com>
2020-11-21 14:58:22 -0800

    Finished findCourse function and tested it

--- Javascript/script.js
+++ Javascript/script.js
@@ -44,22 +44,50 @@ function createCourseArray() {
     return newCourseList
 }
 
-function findCourse() {
-    let courseList = createCourseArray();
+function findCourse(courseList) {
     let code= getValidCode();
     let foundCode = false;
 
     for (let i =0; i <courseList.length; i++) {
         if (courseList[i].code.indexOf(code) > -1) {
             foundCode = true;
-            console.log('changing color of background');
+
+            // change background color here
+            document.querySelector('.acit' + code).style.backgroundColor= 'green';
+            console.log('code found, changing background color!');
+            break;
+
         }
     }
 
     if (!foundCode) {
         console.log('code not found, creating new one!');
-    }
 
+        var p = document.createElement('p');
+        p.classList.add('year');
+        var new_course_year = document.createTextNode('Fall 2020');
+        p.appendChild(new_course_year);
+
+        var p_course_desc = document.createElement("p");
+        var new_course_desc = document.createTextNode("N/A");
+        p_course_desc.classList.add('class_desc');
+        p_course_desc.appendChild(new_course_desc);
+
+        var new_title = document.createElement('a');
+        var new_title_desc = document.createTextNode(code);
+        new_title.appendChild(new_title_desc);
+        new_title.title = code;
+        new_title.href = '';
+
+        var new_div = document.createElement('div');
+        new_div.classList.add('course');
+        new_div.classList.add('acit' + code);
+        new_div.appendChild(new_title);
+        new_div.append(p);
+        new_div.append(p_course_desc);
+
+        document.querySelector('main').appendChild(new_div);
+    }
 }
 
 function getValidCode() {
